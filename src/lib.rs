4b318709225ea5a3e9f0d2a436ea319ef70b7042
Yusufolosun <olosun.m1904582@st.futminna.edu.ng>
2026-09-15 19:50:11 +0100

    implement Cancel instruction  Destructure 6 accounts by position

--- src/lib.rs
+++ src/lib.rs
@@ -27,8 +27,7 @@ pub fn process_instruction(
     match EscrowInstructions::try_from(discriminator)? {
         EscrowInstructions::Make => instructions::process_make_instruction(accounts, data)?,
         EscrowInstructions::Take => instructions::process_take_instruction(accounts, data)?,
-        // TODO (challenge): EscrowInstructions::Cancel
-        EscrowInstructions::Cancel => return Err(ProgramError::InvalidInstructionData),
+        EscrowInstructions::Cancel => instructions::process_cancel_instruction(accounts, data)?,
         EscrowInstructions::MakeV2 => return Err(ProgramError::InvalidInstructionData),
     }
     Ok(())
