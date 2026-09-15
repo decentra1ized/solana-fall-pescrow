48b07afc039b3024d04cd80db8b33824cb11e01c
Janmejai Singh <janmejaisinghvasu20@gmail.com>
2026-09-16 01:24:21 +0530

    challenge 2: cancel the escrow account

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -328,4 +328,120 @@ mod tests {
         assert!(maker_lamports_after_take > maker_lamports_before_take);
         assert_eq!(bump, Pubkey::find_program_address(&[b"escrow", maker.pubkey().as_ref()], &program_id).1);
     }
+
+    #[test]
+    pub fn test_cancel_instruction() {
+        let (mut svm, maker) = setup();
+        let program_id = program_id();
+        println!("Starting Cancel test");
+        println!("Maker: {}", maker.pubkey());
+        let system_program = solana_sdk_ids::system_program::ID;
+        let token_program = TOKEN_PROGRAM_ID;
+        let associated_token_program = ASSOCIATED_TOKEN_PROGRAM_ID.parse::<Pubkey>().unwrap();
+
+        let mint_a = CreateMint::new(&mut svm, &maker)
+            .decimals(6)
+            .authority(&maker.pubkey())
+            .send()
+            .unwrap();
+        let mint_b = CreateMint::new(&mut svm, &maker)
+            .decimals(6)
+            .authority(&maker.pubkey())
+            .send()
+            .unwrap();
+        println!("Mint A: {}", mint_a);
+        println!("Mint B: {}", mint_b);
+        let maker_ata_a = CreateAssociatedTokenAccount::new(&mut svm, &maker, &mint_a)
+            .owner(&maker.pubkey())
+            .send()
+            .unwrap();
+        println!("Maker ATA A: {}", maker_ata_a);
+        MintTo::new(&mut svm, &maker, &mint_a, &maker_ata_a, 1_000_000_000)
+            .send()
+            .unwrap();
+        println!("Minted 1,000,000,000 A to maker");
+
+        let (escrow, bump) = Pubkey::find_program_address(
+            &[b"escrow", maker.pubkey().as_ref()],
+            &program_id,
+        );
+        let vault = spl_associated_token_account::get_associated_token_address(&escrow, &mint_a);
+        println!("Escrow PDA: {} (bump {})", escrow, bump);
+        println!("Vault: {}", vault);
+
+        let make_ix = Instruction {
+            program_id,
+            accounts: vec![
+                AccountMeta::new(maker.pubkey(), true),
+                AccountMeta::new_readonly(mint_a, false),
+                AccountMeta::new_readonly(mint_b, false),
+                AccountMeta::new(escrow, false),
+                AccountMeta::new(maker_ata_a, false),
+                AccountMeta::new(vault, false),
+                AccountMeta::new_readonly(system_program, false),
+                AccountMeta::new_readonly(token_program, false),
+                AccountMeta::new_readonly(associated_token_program, false),
+            ],
+            data: [
+                vec![0u8],
+                100_000_000u64.to_le_bytes().to_vec(),
+                500_000_000u64.to_le_bytes().to_vec(),
+            ]
+            .concat(),
+        };
+        let make_message = Message::new(&[make_ix], Some(&maker.pubkey()));
+        svm.send_transaction(Transaction::new(
+            &[&maker],
+            make_message,
+            svm.latest_blockhash(),
+        ))
+        .unwrap();
+        println!("Make transaction successful");
+
+        let cancel_ix = Instruction {
+            program_id,
+            accounts: vec![
+                AccountMeta::new(maker.pubkey(), true),
+                AccountMeta::new_readonly(mint_a, false),
+                AccountMeta::new(escrow, false),
+                AccountMeta::new(vault, false),
+                AccountMeta::new(maker_ata_a, false),
+                AccountMeta::new_readonly(token_program, false),
+            ],
+            data: vec![2u8],
+        };
+        let cancel_message = Message::new(&[cancel_ix], Some(&maker.pubkey()));
+        let cancel_tx = svm.send_transaction(Transaction::new(
+            &[&maker],
+            cancel_message,
+            svm.latest_blockhash(),
+        ))
+        .unwrap();
+        println!(
+            "Cancel transaction successful; CUs consumed: {}",
+            cancel_tx.compute_units_consumed
+        );
+
+        let maker_a = svm.get_account(&maker_ata_a).unwrap();
+        let maker_a_state = spl_token_2022::state::Account::unpack(&maker_a.data).unwrap();
+        println!("Maker ATA A balance after Cancel: {}", maker_a_state.amount);
+        assert_eq!(maker_a_state.amount, 1_000_000_000);
+
+        let vault_after = svm.get_account(&vault);
+        assert!(vault_after.as_ref().is_none_or(|account| {
+            account.lamports == 0 && account.owner == system_program && account.data.is_empty()
+        }));
+
+        let escrow_after = svm.get_account(&escrow);
+        assert!(escrow_after.as_ref().is_none_or(|account| {
+            account.lamports == 0 && account.owner == system_program && account.data.is_empty()
+        }));
+        println!("Vault closed: {}", vault_after.is_none());
+        println!("Escrow closed: {}", escrow_after.is_none());
+
+        assert_eq!(
+            bump,
+            Pubkey::find_program_address(&[b"escrow", maker.pubkey().as_ref()], &program_id).1
+        );
+    }
 }
